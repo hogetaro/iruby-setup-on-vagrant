3b8af76339f83881e3cc25177c5b7bd5d1790d8a
Hogetaro <hogetaro@gmail.com>
2016-06-18 12:07:00 +0000

    iruby

--- setup-ipython3.sh
+++ setup-ipython3.sh
@@ -1,4 +1,5 @@
 
+. .venv/p3/bin/activate
 sudo apt-get install -y python3-dev virtualenv libzmq3-dev
 sudo apt-get install -y build-essential python-dev
 sudo apt-get install -y python3-pip libzmq3-dev
--- setup-iruby.sh
+++ setup-iruby.sh
@@ -1,6 +1,25 @@
 
+bash ~/lib/setup-ipython3.sh
+
+if ! which anyenv; then
+  bash ~/lib/setup-anyenv.sh
+  exec $SHELL -l $0
+fi
+
+if ! which rbenv; then
+  anyenv install rbenv
+  exec $SHELL -l $0
+fi
+
+if ! ruby -v |grep 2.2.5; then
+  rbenv install 2.2.5
+  rbenv global 2.2.5
+  exec $SHELL -l $0
+fi
+
 sudo apt-get install -y libtool autoconf automake
 
-gem install rbczmq
-gem install iruby
+gem install rbczmq --no-ri --no-rdoc
+gem install iruby --no-ri --no-rdoc
+gem install statsample --no-ri --no-rdoc
 
