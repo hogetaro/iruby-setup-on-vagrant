71aeff0e3113476882e02e0029be86ac15ea1055
Hogetaro <hogetaro@gmail.com>
2016-06-18 23:14:15 +0900

    path dep bug

--- lib/setup-ipython3.sh
+++ lib/setup-ipython3.sh
@@ -1,7 +1,7 @@
 
 bash ~/lib/setup-virtualenv.sh
 
-. .venv/p3/bin/activate
+. ~/.venv/p3/bin/activate
 sudo apt-get install -y python3-dev virtualenv libzmq3-dev
 sudo apt-get install -y build-essential python-dev
 sudo apt-get install -y python3-pip libzmq3-dev
