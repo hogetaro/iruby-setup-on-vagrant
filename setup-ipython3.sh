c9f39f9aac752ba51f39049043f015c06685473d
Hogetaro <hogetaro@gmail.com>
2016-06-18 10:45:50 +0000

    iruby

--- setup-ipython3.sh
+++ setup-ipython3.sh
@@ -1,7 +1,7 @@
 
 sudo apt-get install -y python3-dev virtualenv libzmq3-dev
 sudo apt-get install -y build-essential python-dev
-sudo apt-get install -y libzmq3 libzmq3-dev python3-zmq
+sudo apt-get install -y python3-pip libzmq3-dev
 
 pip3 install zmq
 pip3 install jupyter
---
+++ setup-iruby.sh
@@ -0,0 +1,6 @@
+
+sudo apt-get install -y libtool autoconf automake
+
+gem install rbczmq
+gem install iruby
+
